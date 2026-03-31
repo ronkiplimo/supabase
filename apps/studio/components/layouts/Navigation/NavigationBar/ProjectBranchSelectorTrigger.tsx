3651f017d7bf8c641aa117bb18e782fa747ac952
Francesco Sansalvadore <f.sansalvadore@gmail.com>
2026-03-31 17:41:36 +0200

    add org name tooltip

--- apps/studio/components/layouts/Navigation/NavigationBar/ProjectBranchSelectorTrigger.tsx
+++ apps/studio/components/layouts/Navigation/NavigationBar/ProjectBranchSelectorTrigger.tsx
@@ -1,10 +1,19 @@
 import { ChevronsUpDown, GitBranch } from 'lucide-react'
 import * as React from 'react'
-import { cn, SidebarMenuButton as SidebarMenuButtonComponent } from 'ui'
+import type { Organization } from 'types'
+import {
+  cn,
+  SidebarMenuButton as SidebarMenuButtonComponent,
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from 'ui'
+
+import { getSelectedOrgInitial } from './ProjectBranchSelector.utils'
 
 export interface ProjectBranchSelectorTriggerProps {
   displayProjectName: string
-  selectedOrgInitial: string
+  selectedOrg: Organization
   isBranch: boolean
   isProductionBranch: boolean
   branchDisplayName: string
@@ -18,44 +27,47 @@ export const ProjectBranchSelectorTrigger = React.forwardRef<
       React.ComponentPropsWithoutRef<typeof SidebarMenuButtonComponent>,
       keyof ProjectBranchSelectorTriggerProps
     >
->(
-  (
-    { displayProjectName, selectedOrgInitial, isBranch, branchDisplayName, ...buttonProps },
-    ref
-  ) => {
-    return (
-      <SidebarMenuButtonComponent
-        ref={ref}
-        size="lg"
-        className="group py-1 gap-1.5 w-full flex h-auto text-left data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground touch-manipulation"
-        {...buttonProps}
-      >
-        <div className="relative flex h-8 aspect-square shrink-0 items-center bg-background-muted group-hover:border-stronger justify-center rounded border border-strong text-xs">
-          {selectedOrgInitial}
-        </div>
+>(({ displayProjectName, selectedOrg, isBranch, branchDisplayName, ...buttonProps }, ref) => {
+  const selectedOrgInitial = getSelectedOrgInitial(selectedOrg.name)
+
+  return (
+    <SidebarMenuButtonComponent
+      ref={ref}
+      size="lg"
+      className="group py-1 gap-1.5 w-full flex h-auto text-left data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground touch-manipulation"
+      {...buttonProps}
+    >
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <div className="relative flex h-8 aspect-square shrink-0 items-center bg-background-muted group-hover:border-stronger justify-center rounded border border-strong text-xs">
+            {selectedOrgInitial}
+          </div>
+        </TooltipTrigger>
+        <TooltipContent className="px-2">{selectedOrg.name}</TooltipContent>
+      </Tooltip>
+
+      <div className="text-left flex-grow min-w-0">
         <div className="text-left flex-grow min-w-0">
-          <div className="text-left flex-grow min-w-0">
-            <div className="w-full truncate text-foreground leading-tight max-w-[250px]">
-              {displayProjectName}
-            </div>
-            <div
-              className={cn(
-                'flex items-center gap-0.5',
-                isBranch ? 'text-foreground-lighter' : 'text-warning'
-              )}
-            >
-              <GitBranch className="shrink-0 size-3" strokeWidth={1.5} />
-              <span className="truncate min-w-0 leading-tight text-xs">{branchDisplayName}</span>
-            </div>
+          <div className="w-full truncate text-foreground leading-tight max-w-[250px]">
+            {displayProjectName}
+          </div>
+          <div
+            className={cn(
+              'flex items-center gap-0.5',
+              isBranch ? 'text-foreground-lighter' : 'text-warning'
+            )}
+          >
+            <GitBranch className="shrink-0 size-3" strokeWidth={1.5} />
+            <span className="truncate min-w-0 leading-tight text-xs">{branchDisplayName}</span>
           </div>
         </div>
+      </div>
 
-        <ChevronsUpDown
-          strokeWidth={1.5}
-          className="ml-auto text-foreground-lighter !w-4 !h-4 md:opacity-0 md:group-hover:opacity-100 transition-opacity"
-        />
-      </SidebarMenuButtonComponent>
-    )
-  }
-)
+      <ChevronsUpDown
+        strokeWidth={1.5}
+        className="ml-auto text-foreground-lighter !w-4 !h-4 md:opacity-0 md:group-hover:opacity-100 transition-opacity"
+      />
+    </SidebarMenuButtonComponent>
+  )
+})
 ProjectBranchSelectorTrigger.displayName = 'ProjectBranchSelectorTrigger'

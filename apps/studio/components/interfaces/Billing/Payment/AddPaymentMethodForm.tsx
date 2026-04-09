4e68550f38550a4717b98316bc84fbb8fa0fcc62
Ignacio Dobronich <ignacio@dobronich.com>
2026-04-09 14:10:44 -0300

    Fix undefined taxId

--- apps/studio/components/interfaces/Billing/Payment/AddPaymentMethodForm.tsx
+++ apps/studio/components/interfaces/Billing/Payment/AddPaymentMethodForm.tsx
@@ -73,7 +73,7 @@ const AddPaymentMethodForm = ({ onCancel, onConfirm }: AddPaymentMethodFormProps
         formValues.address &&
         (!isEqual(formValues.address, customerProfile?.address) ||
           customerProfile?.billing_name !== formValues.customerName)
-      const taxIdChanged = !isEqual(formValues.taxId, taxId)
+      const taxIdChanged = taxId !== undefined && !isEqual(formValues.taxId, taxId)
 
       if (addressChanged || taxIdChanged) {
         try {
